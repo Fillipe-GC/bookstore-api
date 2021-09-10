3dc4f5098b35a7bb7c60aa6513771daf1e816c3c
Fillipe-GC <fillipe.gc@gmail.com>
2021-09-10 10:01:35 -0300

    Exceção personalizada para objeto não encontrado (Aula 11)

--- src/main/java/com/datagrupo/bookstore/service/CategoriaService.java
+++ src/main/java/com/datagrupo/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.datagrupo.bookstore.domain.Categoria;
 import com.datagrupo.bookstore.repositories.CategoriaRepository;
+import com.datagrupo.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ",Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
