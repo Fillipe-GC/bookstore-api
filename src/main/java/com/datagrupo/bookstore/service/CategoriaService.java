b3ea07e8e3c2763edd468d4488bb72303c2f4072
Fillipe-GC <fillipe.gc@gmail.com>
2021-09-10 09:25:24 -0300

    FindById para Categoria (Aula 10)

---
+++ src/main/java/com/datagrupo/bookstore/service/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.datagrupo.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.datagrupo.bookstore.domain.Categoria;
+import com.datagrupo.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	public Categoria findById(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
