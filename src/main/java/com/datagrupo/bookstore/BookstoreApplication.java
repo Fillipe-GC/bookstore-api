7b92d696a53270a248bcf8cff6f6b426f5773b15
Fillipe-GC <fillipe.gc@gmail.com>
2021-09-10 00:19:26 -0300

    Perfil de teste implementado (Aula 08)

--- src/main/java/com/datagrupo/bookstore/BookstoreApplication.java
+++ src/main/java/com/datagrupo/bookstore/BookstoreApplication.java
@@ -1,40 +1,13 @@
 package com.datagrupo.bookstore;
 
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.datagrupo.bookstore.domain.Categoria;
-import com.datagrupo.bookstore.domain.Livro;
-import com.datagrupo.bookstore.repositories.CategoriaRepository;
-import com.datagrupo.bookstore.repositories.LivroRepository;
-
 @SpringBootApplication
-public class BookstoreApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
-
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-
-		cat1.getLivros().addAll(Arrays.asList(l1));
-
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-
 	}
 
 }
